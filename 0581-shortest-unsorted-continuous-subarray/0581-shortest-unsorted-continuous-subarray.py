ef6ce1a898ab0a7e71d244c84af5d3e2ee98580d
santosh-1394 <santoshyallapragada@gmail.com>
2026-07-24 16:41:46 +0530

    Time: 31 ms (29.25%), Space: 13.2 MB (90.48%) - LeetHub

---
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findUnsortedSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        start, end = -1, -2  
+        min_val, max_val = nums[-1], nums[0]
+        
+        
+        for i in range(1, n):
+            max_val = max(max_val, nums[i])
+            if nums[i] < max_val:
+                end = i
+        
+        
+        for i in range(n-2, -1, -1):
+            min_val = min(min_val, nums[i])
+            if nums[i] > min_val:
+                start = i
+        
+        return end - start + 1
